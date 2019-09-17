15ec8e827068aa65d9ec6531016529fa7a5f86ff
Admin <admin@admins-mbp.localdomain>
2019-09-17 09:29:33 +0200

    Updates

---
+++ precache-manifest.854f470ec9dbf48bc1b3bf285e201fca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fef3b0ec54c753745d0ef9f80304167",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9c29ddfbe2ac20f0f378",
+    "url": "/monsters-rolodex/static/css/main.3acca2f2.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "9c29ddfbe2ac20f0f378",
+    "url": "/monsters-rolodex/static/js/main.21cc3ec5.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
